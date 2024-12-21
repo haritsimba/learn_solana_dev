b7267131d54de34fd703342552af00d575cb9603
haritsimba <haritsimbafranck@gmail.com>
2024-12-21 17:50:07 +0300

    modif : (module 2) read data from solana network

--- index.ts
+++ index.ts
@@ -1,3 +1,31 @@
 import {CryptographySolanaNetwork} from "./CryptographySolanaNetwork";
-//module one
-CryptographySolanaNetwork.main()
+import {ReadDataSolanaNetwork} from "./ReadDataSolanaNetwork";
+//#################################################################################################//
+/*
+ * Module one : Cryptography and the Solana Network
+ * Summary :
+ A keypair is a matching pair of public key and secret key.
+ The public key is used as an “address” that points to an account on the Solana network. A public key can be shared with anyone.
+ The secret key is used to verify authority over the account. As the name suggests, you should always keep secret keys secret.
+ @solana/web3.js provides helper functions for creating a brand new keypair, or for constructing a keypair using an existing secret key.
+ */
+
+//CryptographySolanaNetwork.main()
+
+//#################################################################################################//
+
+//#################################################################################################//
+/*
+   * Module two : Read Data From The Solana Network
+   * Summary :
+    SOL is the name of Solana's native token. Each SOL is made from 1 billion Lamports.
+    Accounts store tokens, NFTs, programs, and data. For now, we'll focus on accounts that store SOL.
+    Addresses point to accounts on the Solana network. Anyone can read the data at a given address. Most addresses are also public keys.
+
+ */
+
+ReadDataSolanaNetwork.main()
+
+//#################################################################################################//
+
+//#################################################################################################//
