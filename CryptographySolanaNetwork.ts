dfb48c0fb6e5fade05cda4d3bc6c2c80864a2e41
haritsimba <haritsimbafranck@gmail.com>
2024-12-21 17:30:01 +0300

    modif : module one

--- CryptographySolanaNetwork.ts
+++ CryptographySolanaNetwork.ts
@@ -2,15 +2,23 @@ import { Keypair } from "@solana/web3.js";
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-class CryptographySolanaNetwork{
+
+export class CryptographySolanaNetwork{
+    /**
+     * generate keypair
+     */
     public static createKeypair(): Keypair{
         const keypair = Keypair.generate();
 
         console.log(`The public key is: `, keypair.publicKey.toBase58());
         console.log(`The secret key is: `, keypair.secretKey);
+
         return keypair
     }
 
+    /**
+     * load the transaction payer keypair stored in the keys directory
+     */
     public static loadPayerSecretKey(): Keypair{
         const wallet = getKeypairFromEnvironment("SECRET_KEY");
 
@@ -21,6 +29,10 @@ class CryptographySolanaNetwork{
         return wallet;
     }
 
+    public static main(){
+        this.createKeypair()
+        this.loadPayerSecretKey()
+    }
 }
 
 
